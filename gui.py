823891f5cc05d05a7a3f78e28cb40f0ba4f43658
vossenwout <vossen.w@hotmail.com>
2019-09-06 14:05:50 +0200

    sec

--- gui.py
+++ gui.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from PIL import ImageTk, Image
+import os
+
 
 # application is a frame which 2 stacked frames (bottom and top frame), master is the window for the frames
 class Application(tk.Frame):
@@ -19,6 +22,10 @@ class Application(tk.Frame):
         # creating the widgets
         self.create_widgets()
 
+        # loading images
+        self.open_image()
+
+
     def masterWindow_setup(self):
         self.master.title("Brew Master 1.0")
         self.master.minsize(500, 500)
@@ -26,13 +33,25 @@ class Application(tk.Frame):
 
 
     def create_widgets(self):
-        self.launchButton = tk.Button(self.botFrame, text="LAUNCH BREWMASTER", width=25,
-                                      command = self.launch_main_menu)
+        quitButton = tk.Button(self.botFrame, text="QUIT", width=25, fg="red", command=self.master.destroy)
+        launchButton = tk.Button(self.botFrame, text="LAUNCH BREWMASTER", width=25,command = self.launch_main_menu)
+        welcomeLabel = tk.Label(self.botFrame, text="Welcome to brewmasters \n v1.0, by Pookie",font ="Times 18" ,height=5)
 
-        self.launchButton.pack(side="top")
-        self.quitButton = tk.Button(self.botFrame, text="QUIT", width=25, fg="red"
-                                                    ,command=self.master.destroy)
-        self.quitButton.pack(side="bottom")
+        welcomeLabel.grid(row=0, column=0)
+        quitButton.grid(row=2, column=0)
+        launchButton.grid(row=1, column=0)
+
+
+    def open_image(self):
+
+        path = "images/brewimage.jpg"
+        image = Image.open(path)
+        image = image.resize((400, 300), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(image)
+
+        label = tk.Label(self.topFrame, image=img)
+        label.image = img
+        label.grid(row=0, column=0)
 
     def launch_main_menu(self):
         print("next windnppow")
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import  tkinter as tk
 from gui import Application
+from PIL import ImageTk, Image
 
 def main():
     root = tk.Tk()
     app = Application(master=root)
+
     app.mainloop()
 
 if __name__ == "__main__":
