01faa9dd492e7f525632e5b20a45ec125d7109cf
vossenwout <vossen.w@hotmail.com>
2019-09-07 12:13:04 +0200

    sec

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from PIL import ImageTk, Image
 
 # application is a frame which 2 stacked frames (bottom and top frame), master is the window for the frames
+############################################################################################################################
 class WelcomeScreen(tk.Frame):
     def __init__(self, master=None):
 
@@ -28,6 +29,7 @@ class WelcomeScreen(tk.Frame):
     def masterWindow_setup(self):
         self.master.title("Brew Master 1.0")
         self.master.minsize(500, 500)
+        self.master.resizable(False, False)
         self.pack()
 
 
@@ -57,10 +59,11 @@ class WelcomeScreen(tk.Frame):
     def launch_main_menu(self):
         self.topFrame.destroy()
         self.botFrame.destroy()
-        menu = menu_screen(self.master)
+        menu = MenuScreen(self.master)
 
 
-class menu_screen(tk.Frame):
+#############################################################################################
+class MenuScreen(tk.Frame):
     def __init__(self, master=None):
 
         # setting up the master window
@@ -68,8 +71,52 @@ class menu_screen(tk.Frame):
         self.master = master
         self.masterWindow_setup()
 
+        self.topFrame = tk.Frame(master)
+        self.topFrame.pack()
+
+        self.botFrame = tk.Frame(master)
+        self.botFrame.pack(side="bottom")
+
+        self.create_widgets()
+        self.open_image()
+
+
     def masterWindow_setup(self):
         self.master.title("Brew Master 1.0")
-        self.master.minsize(1000, 500)
+        self.master.minsize(500, 500)
+        self.master.resizable(False, False)
         self.pack()
+
+    def create_widgets(self):
+        self.recipesButton = tk.Button(self.botFrame, text="RECIPES", width=25,
+                                      command=self.launch_recipes_menu)
+        self.calculationsButton = tk.Button(self.botFrame, text="CALCULATIONS", width=25,
+                                      command=self.launch_calculations_menu)
+
+        self.quitButton = tk.Button(self.botFrame, text="QUIT", width=25, fg="red", command=self.master.destroy)
+        self.welcomeLabel = tk.Label(self, text="Chose what you wanna do", font="Times 18",
+                                     height=5)
+
+        self.recipesButton.grid(row=1, column=0)
+        self.quitButton.grid(row=2, column=1)
+        self.calculationsButton.grid(row=1, column=1)
+
+
+    def launch_recipes_menu(self):
+        print("next")
+
+    def launch_calculations_menu(self):
+        print("next")
+
+        # image on welcome screen
+
+    def open_image(self):
+        path = "images/brewimage2.jpg"
+        self.image = Image.open(path)
+        self.image = self.image.resize((400, 300), Image.ANTIALIAS)
+        self.img = ImageTk.PhotoImage(self.image)
+
+        self.label = tk.Label(self, image=self.img)
+        self.label.image = self.img
+        self.label.grid(row=0, column=0)
 
